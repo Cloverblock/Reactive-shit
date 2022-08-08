6431f2576fa402f95e528913f4f9b581a516eb44
Cloverblock <69807959+Cloverblock@users.noreply.github.com>
2022-08-08 15:30:47 +0300

    Элементы, PropTypes и тд. 2 часть

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import {createRoot} from "react-dom/client"
 
 const node = document.getElementById('root');
 class Post extends Component {
@@ -11,7 +12,7 @@ class Post extends Component {
                           //Неужели и в нормальных проектах также сложно всё
       },
       React.createElement(
-        'h1',
+        'h2',
         {
           className: 'postAuthor',
           id: this.props.id
@@ -29,6 +30,72 @@ class Post extends Component {
     );
   }
 }
+
+class CreateComment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+      user:''
+    };
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleUserChange(event) {
+    const val = event.target.value;
+    this.setState(() => ({
+      user: val
+    }));
+  }
+    handleTextChange(event) {
+      const val = event.target.value;
+      this.setState(() => ({
+        content: val
+      }));
+    }
+    handleSubmit(event) {
+      event.preventDefault();
+      this.props.onCommentSubmit({
+        user: this.state.user.trim(),
+        content: this.state.content.trim()
+      });
+      this.setState(() => ({
+        user:'',
+        content: ''
+      }));
+    }
+
+    render() {
+      return React.createElement (
+        'form',
+        {
+          className: 'createComment',
+          onSubmit: this.handleSubmit
+        },
+        React.createElement('input', {
+          type:'text',
+          placeholder: 'Your name',
+          value: this.state.user,
+          onChange: this.handleUserChange
+        }),
+        React.createElement('input', {
+          type: 'text',
+          placeholder:'Thoughts?',
+          value: this.state.content,
+          onChange: this.handleTextChange
+        }),
+        React.createElement('input', {
+          type: 'submit',
+          value:'Post'
+        })
+      );
+    }
+  }
+CreateComment.propTypes = {
+  onCommentSubmit: PropTypes.func.isRequired,
+  content: PropTypes.string
+};
 
     class Comment extends Component {
       render() {
@@ -57,31 +124,108 @@ class Post extends Component {
   }
 }
 
-const ReactDOM = require('react-dom') //Вот этой херни быть здесь не должно(даже в книге нет), но без нее выдает ошибку на последней строке
+const ReactDOM = require('react-dom') 
 
 Post.propTypes = {
-  user: PropTypes.string.isRequired,    //Тут впринципе всё понятно, только не понятно как запомнить все это
-  content: PropTypes.string.isRequired, // Типо вот эти 3 строки мне ясно как работают, но если дело дойдет до нормальной работы
-  id: PropTypes.number.isRequired       // То я писю пососу и не вспомнию что вот так вот всё пишется
+  user: PropTypes.string.isRequired,  
+  content: PropTypes.string.isRequired, 
+  id: PropTypes.number.isRequired       
 };
 
 Comment.propTypes ={
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   user: PropTypes.string.isRequired
 };
 
-const App = React.createElement(Post, {
-  id: 1,
-  content: ' сказал: Я ебал меня сосали!',
-  user: 'Хуюзер' // Такой же прикол, что и в коменте выше
-},
-React.createElement(Comment, {
-  id: 2,
-  user: 'Бомжузер ',
-  content: 'прокомментировал: А меня забыли ((('
-})
-);
+const App = React.createElement(CreateComment);
+
+const data = {
+  post: {
+    id: 123,
+    content:': "Да не боись, не ебанёт" - Робер Оппенгеймер',
+    user: 'Samuel Jackson'
+  },
+  comments: [
+    {
+      id: 0,
+      user: 'DaiVinci',
+      content: ': Глубоко',
+    },
+    {
+      id: 1,
+      user: 'Нагибатор228',
+      content: ': Я ебу собак',
+    },
+    {
+      id: 2,
+      user: 'ТвояМама',
+      content: ': @Нагибатор228 Всегда готов',
+    },
+    {
+      id: 3,
+      user: 'РыжийБог',
+      content: ': @ТвояМама трахнуть сразу несколько котов',
+    },
+    {
+      id: 4,
+      user: 'ЧоткийПоцык',
+      content: ': Долбоебы бля',
+    },
+  ],
+};
+
+class CommentBox extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: this.props.comments
+    };
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+  handleCommentSubmit(comment) {
+    const comments = this.state.comments;
+    comment.id = Date.now();
+    const newComments = comments.concat([comment]);
+    this.setState ({
+      comments:newComments
+    });
+  }
+  render(){
+    return React.createElement (
+      'div',
+      {
+        className: 'commentBox'
+      },
+      React.createElement(Post, {
+        id: this.props.post.id,
+        content: this.props.post.content,
+        user: this.props.post.user
+      }),
+      this.state.comments.map(function(comment){
+        return React.createElement(Comment, {
+          key: comment.id,
+          id: comment.content,
+          content: comment.content,
+          user: comment.user
+        });
+      }),
+      React.createElement(CreateComment, {
+        onCommentSubmit: this.handleCommentSubmit
+      })
+    );
+  }
+}
+CommentBox.propTypes = {
+  post: PropTypes.object,
+  comments: PropTypes.arrayOf(PropTypes.object)
+};
 
 
-ReactDOM.render(App, node);
+ReactDOM.render(
+  React.createElement(CommentBox, {
+    comments: data.comments,
+    post: data.post
+  }),
+  node
+)
