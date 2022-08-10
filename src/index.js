e3aeb17307650771a8422c6df4b8a89db81e4613
Cloverblock <69807959+Cloverblock@users.noreply.github.com>
2022-08-10 18:11:25 +0300

    Стили ебаные

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-
+import './index.css';
 
 const node = document.getElementById('root');
 class Post extends Component {
